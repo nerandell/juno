ee78ef5c9f40b775a09b9982573597217b7ba418
Ankit Chandawala <ankitchandawala@gmail.com>
2022-03-10 00:00:54 +0000

    Add macros to print to VGA Buffer

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,3 +1,4 @@
+use core::fmt;
 use core::fmt::Write;
 use volatile::Volatile;
 use lazy_static::lazy_static;
@@ -131,4 +132,19 @@ impl VGABufferWriter {
             })
         }
     }
+}
+
+pub fn _print(args: fmt::Arguments) {
+    WRITER.lock().write_fmt(args).unwrap();
+}
+
+#[macro_export]
+macro_rules! println {
+    () => (print!("\n"));
+    ($($arg:tt)*) => (print!("{}\n", format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
 }

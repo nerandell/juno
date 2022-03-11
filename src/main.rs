96573516035289475696609fc717bcbc4d4723ce
Ankit Chandawala <ankitchandawala@gmail.com>
2022-03-11 10:13:36 +0000

    Exit QEMU after test run

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,35 @@
 // Define custom test framework
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
+// Override the function name that test runner creates to
+// execute since it cannot be main because of the no_main
+// attribute
 #![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
 
 use core::panic::PanicInfo;
+use x86_64::instructions::port::Port;
+
+#[derive(Debug)]
+#[repr(u32)]
+enum QemuExitCode {
+    Success = 0x10,
+    Failure = 0x11
+}
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+// Write to Port mapped IO to exit QEMU.
+fn exit_qemu(exit_code: QemuExitCode) {
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
 
 // Custom test runner
@@ -23,12 +42,14 @@ fn test_runner(tests: &[&dyn Fn()]) {
     for test in tests {
         test();
     }
+
+    exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
 fn dummy_assertion() {
     println!("Dummy assertion test");
-    assert_eq!(1, 2);
+    assert_eq!(1, 1);
     println!("[OK]");
 }
 

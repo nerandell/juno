bdf67a9b57818da10ce161a8bd986f0e8a8c440d
Ankit Chandawala <ankitchandawala@gmail.com>
2022-03-11 09:16:54 +0000

    Define custom test framework

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 #![no_std]
 #![no_main]
+
+// Define custom test framework
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+
 mod vga_buffer;
 
 use core::panic::PanicInfo;
@@ -14,4 +19,13 @@ fn panic(info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
     println!("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
     loop {}
+}
+
+// Custom test runner
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
 }

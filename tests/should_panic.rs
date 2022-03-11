41ad4d2ea0667579945a4fa300b356425d2a2207
Ankit Chandawala <ankitchandawala@gmail.com>
2022-03-11 16:36:02 +0000

    Simplify tests for panic handling

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -2,14 +2,19 @@
 #![no_main]
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
-#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 use juno::{QemuExitCode, exit_qemu, DebugTest, println, serial_println};
 
+fn should_fail() {
+    assert_eq!(0, 1);
+}
+
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    test_main();
+    should_fail();
+    serial_println!("Test did not panic!");
+    exit_qemu(QemuExitCode::Failure);
 
     loop {}
 }
@@ -19,22 +24,10 @@ fn panic(info: &PanicInfo) -> ! {
     serial_println!("{}", info);
     serial_println!("[OK]");
     exit_qemu(QemuExitCode::Success);
+
     loop {}
 }
 
-pub fn test_runner(tests: &[&dyn DebugTest]) {
-    serial_println!("Running tests {}", tests.len());
-    for test in tests {
-        test.run();
-        serial_println!("Test did not panic!");
-        exit_qemu(QemuExitCode::Failure);
-    }
-    exit_qemu(QemuExitCode::Success);
-}
 
-#[test_case]
-fn test_println() {
-    assert_eq!(0, 1);
-}
 
 
